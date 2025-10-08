b691643990f9ce7fbea0d9b81530a919ef5fa0ac
ndizeye alain <alainndizeye11@gmail.com>
2025-10-08 20:23:40 +0200

    created the first method and variables in the databasehelper file

--- app/src/main/java/com/example/assignment2/database/DatabaseHelper.java
+++ app/src/main/java/com/example/assignment2/database/DatabaseHelper.java
@@ -1,4 +1,20 @@
 package com.example.assignment2.database;
 
-public class DatabaseHelper {
-}
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "StudentDB";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_STUDENTS = "students";
+
+    // Column names
+    private static final String KEY_ID = "id";
+    private static final String KEY_FULL_NAME = "full_name";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_GENDER = "gender";
+    private static final String KEY_NEWSLETTER = "newsletter";
