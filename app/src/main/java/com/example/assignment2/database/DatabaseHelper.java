b01d4261085cf3d4429301c057942d83647bd9b2
ndizeye alain <alainndizeye11@gmail.com>
2025-10-08 20:26:58 +0200

    created tables and entities through sqlite

--- app/src/main/java/com/example/assignment2/database/DatabaseHelper.java
+++ app/src/main/java/com/example/assignment2/database/DatabaseHelper.java
@@ -18,3 +18,43 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String KEY_PASSWORD = "password";
     private static final String KEY_GENDER = "gender";
     private static final String KEY_NEWSLETTER = "newsletter";
+    // Create table query
+    private static final String CREATE_TABLE_STUDENTS = "CREATE TABLE " + TABLE_STUDENTS + "("
+            + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + KEY_FULL_NAME + " TEXT,"
+            + KEY_EMAIL + " TEXT UNIQUE,"
+            + KEY_PASSWORD + " TEXT,"
+            + KEY_GENDER + " TEXT,"
+            + KEY_NEWSLETTER + " INTEGER DEFAULT 0"
+            + ")";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_STUDENTS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_STUDENTS);
+        onCreate(db);
+    }
+
+    // Add a new student
+    public long addStudent(String fullName, String email, String password, String gender, boolean newsletter) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_FULL_NAME, fullName);
+        values.put(KEY_EMAIL, email);
+        values.put(KEY_PASSWORD, password);
+        values.put(KEY_GENDER, gender);
+        values.put(KEY_NEWSLETTER, newsletter ? 1 : 0);
+
+        long id = db.insert(TABLE_STUDENTS, null, values);
+        db.close();
+        return id;
+    }
+
