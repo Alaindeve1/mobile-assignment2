e774137d57d478023e939052bf92f1a39bd48785
ndizeye alain <alainndizeye11@gmail.com>
2025-10-10 09:43:39 +0200

    finalised the modal class and debugged errors

--- app/src/main/java/com/example/assignment2/models/Student.java
+++ app/src/main/java/com/example/assignment2/models/Student.java
@@ -49,3 +49,22 @@ public class Student implements Serializable {
     public String getPhone() { return phone; }
     public String getGender() { return gender; }
     public String getPassword() { return password; }
+    // Setters
+    public void setId(long id) { this.id = id; }
+    public void setStudentId(String studentId) { this.studentId = studentId; }
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public void setGender(String gender) { this.gender = gender; }
+    public void setPassword(String password) { this.password = password; }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", studentId='" + studentId + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
