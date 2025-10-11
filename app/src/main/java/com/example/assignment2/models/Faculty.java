a00818515fd9aa558b8a0d7800cd577e1b757c44
ndizeye alain <alainndizeye11@gmail.com>
2025-10-11 21:45:58 +0200

    started working on the faculty entity

--- app/src/main/java/com/example/assignment2/models/Faculty.java
+++ app/src/main/java/com/example/assignment2/models/Faculty.java
@@ -1,4 +1,8 @@
 package com.example.assignment2.models;
 
-public class Faculty {
-}
+import java.io.Serializable;
+
+/**
+ * Faculty Model Class
+ * Represents a faculty/department in the university
+ */
