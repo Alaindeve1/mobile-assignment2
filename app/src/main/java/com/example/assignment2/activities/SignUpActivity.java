e4250282284dcf9d78e55668129b65ad5c4d266a
ndizeye alain <alainndizeye11@gmail.com>
2025-11-07 14:20:02 +0200

    migrated the file to new directory and updated references

--- app/src/main/java/com/example/assignment2/activities/SignUpActivity.java
+++ app/src/main/java/com/example/assignment2/activities/SignUpActivity.java
@@ -1,4 +1,4 @@
-package com.example.assignment2;
+package com.example.assignment2.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,6 +19,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.assignment2.R;
 import com.example.assignment2.database.DatabaseHelper;
 import com.example.assignment2.models.Faculty;
 import com.example.assignment2.models.Student;
