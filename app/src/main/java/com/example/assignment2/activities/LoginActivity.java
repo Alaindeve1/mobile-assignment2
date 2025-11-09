3c0c1d88c48b47b3d1da3fba347ea8b0c172e134
ndizeye alain <alainndizeye11@gmail.com>
2025-11-09 21:48:55 +0200

    changed the directory of the file for better file structure

--- app/src/main/java/com/example/assignment2/activities/LoginActivity.java
+++ app/src/main/java/com/example/assignment2/activities/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.assignment2;
+package com.example.assignment2.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +14,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.assignment2.R;
 import com.example.assignment2.database.DatabaseHelper;
 import com.example.assignment2.models.Student;
 
