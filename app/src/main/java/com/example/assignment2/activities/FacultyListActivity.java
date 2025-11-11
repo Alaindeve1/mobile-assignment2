a9db19748ed16645a23c9168e37eaa2e30c6eba3
ndizeye alain <alainndizeye11@gmail.com>
2025-11-11 21:20:53 +0200

    changed the directory and updated paths for faculty list activity

--- app/src/main/java/com/example/assignment2/activities/FacultyListActivity.java
+++ app/src/main/java/com/example/assignment2/activities/FacultyListActivity.java
@@ -1,14 +1,13 @@
-package com.example.assignment2;
+package com.example.assignment2.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.assignment2.R;
 import com.example.assignment2.database.DatabaseHelper;
 import com.example.assignment2.models.Faculty;
